8c6355cd7d4f9d95a2a26820f6dff159534034b1
noajansma <69960128+noajansma@users.noreply.github.com>
2020-08-23 22:47:23 +0200

    speed Certificate1

--- js/data.js
+++ js/data.js
@@ -2,10 +2,10 @@ var data = [
     {
         "x0" : 100,
         "y0" : 100,
-        "vx" : 0.5,
+        "vx" : 0.3,
         "vy" : 0.2,
         "radius" : 40,
         "image" : "Certificate-1-EmileAlmekinders.png",
         "t0" : 1594818061200
-    },
+    }
 ];
