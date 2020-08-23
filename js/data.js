e31fb642f7ed4a7a7e70e65102d470d9a0c72034
noajansma <69960128+noajansma@users.noreply.github.com>
2020-08-23 23:43:39 +0200

    added cert 2 and 3, changed variables

--- js/data.js
+++ js/data.js
@@ -2,10 +2,29 @@ var data = [
     {
         "x0" : 55,
         "y0" : 55,
-        "vx" : 0.3,
+        "vx" : 0.1,
         "vy" : 0.2,
-        "radius" : 40,
-        "image" : "Certificate-1-EmileAlmekinders.png",
+        "radius" : 20,
+        "image" : "certificate-1-EmileAlmekinders.png",
+        "t0" : 1594818061200
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.2,
+        "radius" : 10,
+        "image" : "certificate-2-MarcoTiberio.png",
+        "t0" : 1594818061200
+    },
+    {
+        "x0" : 100,
+        "y0" : 100,
+        "vx" : 0.1,
+        "vy" : 0.2,
+        "radius" : 15,
+        "image" : "certificate-3-CarolineNevejan.png",
         "t0" : 1594818061200
     }
+
 ];
