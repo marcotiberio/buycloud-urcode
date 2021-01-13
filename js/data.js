a75564626eba429d1a728487c95ab2cbc712a795
noajansma <69960128+noajansma@users.noreply.github.com>
2021-01-13 19:10:41 +0100

    Cert MAartjeW added to us

--- js/data.js
+++ js/data.js
@@ -124,6 +124,16 @@ var data = [
         "image" : "certificate-14-ThomasErdbrink-12.png",
         "t0" : 1594818061200   
      
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 15,
+        "image" : "certificateMaartjeWortel-12.png",
+        "t0" : 1594818061200   
+     
     }
     
 
