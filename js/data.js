3dc0af2b98908d5568c35f22527b8eddffb67420
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2021-02-08 13:55:54 +0100

    all of them

--- js/data.js
+++ js/data.js
@@ -1,120 +1,120 @@
 var data = [
-    // {
-    //     "x0" : 55,
-    //     "y0" : 55,
-    //     "vx" : 0.1,
-    //     "vy" : 0.2,
-    //     "radius" : 30,
-    //     "image" : "Certificate-1-EmileAlmekinders.png",
-    //     "t0" : 1594818061200
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.2,
-    //     "radius" : 25,
-    //     "image" : "certificate-2-MarcoTiberio.png",
-    //     "t0" : 1594818061200
-    // },
-    // {
-    //     "x0" : 100,
-    //     "y0" : 100,
-    //     "vx" : 0.1,
-    //     "vy" : 0.2,
-    //     "radius" : 20,
-    //     "image" : "certificate-3-CarolineNevejan.png",
-    //     "t0" : 1594818061200
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.2,
-    //     "radius" : 15,
-    //     "image" : "certificate-5-MaartjeNevejan.png",
-    //     "t0" : 1594818061200
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 16,
-    //     "image" : "certificate-6-ReinJansma-06.png",
-    //     "t0" : 1594818061200   
+    {
+        "x0" : 55,
+        "y0" : 55,
+        "vx" : 0.1,
+        "vy" : 0.2,
+        "radius" : 30,
+        "image" : "Certificate-1-EmileAlmekinders.png",
+        "t0" : 1594818061200
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.2,
+        "radius" : 25,
+        "image" : "certificate-2-MarcoTiberio.png",
+        "t0" : 1594818061200
+    },
+    {
+        "x0" : 100,
+        "y0" : 100,
+        "vx" : 0.1,
+        "vy" : 0.2,
+        "radius" : 20,
+        "image" : "certificate-3-CarolineNevejan.png",
+        "t0" : 1594818061200
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.2,
+        "radius" : 15,
+        "image" : "certificate-5-MaartjeNevejan.png",
+        "t0" : 1594818061200
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 16,
+        "image" : "certificate-6-ReinJansma-06.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 14,
-    //     "image" : "certificate-7-TeddyCherim-07.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 14,
+        "image" : "certificate-7-TeddyCherim-07.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 8,
-    //     "image" : "Certificate-8-KoendeBoer-06.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 8,
+        "image" : "Certificate-8-KoendeBoer-06.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 8,
-    //     "image" : "certificate-9-SterreJoor-07.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 8,
+        "image" : "certificate-9-SterreJoor-07.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 8,
-    //     "image" : "certificate-10-JasmineKarimova-08.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 8,
+        "image" : "certificate-10-JasmineKarimova-08.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 55,
-    //     "y0" : 55,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 8,
-    //     "image" : "certificate-11-NinaSomer-09.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 55,
+        "y0" : 55,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 8,
+        "image" : "certificate-11-NinaSomer-09.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 13,
-    //     "image" : "certificate-12-KirikoMechanicus-10.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 13,
+        "image" : "certificate-12-KirikoMechanicus-10.png",
+        "t0" : 1594818061200   
      
-    // },
-    // {
-    //     "x0" : 1,
-    //     "y0" : 1,
-    //     "vx" : 0.1,
-    //     "vy" : 0.1,
-    //     "radius" : 15,
-    //     "image" : "certificate-13-RamsesWalon-11.png",
-    //     "t0" : 1594818061200   
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 15,
+        "image" : "certificate-13-RamsesWalon-11.png",
+        "t0" : 1594818061200   
      
-    // },
+    },
     {
         "x0" : 1,
         "y0" : 1,
