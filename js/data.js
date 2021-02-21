51355abf2dcdea710af77d852750696c8ba68857
noajansma <69960128+noajansma@users.noreply.github.com>
2021-02-21 13:39:25 +0100

    different order

--- js/data.js
+++ js/data.js
@@ -10,6 +10,16 @@ var data = [
      
     },
     {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.5,
+        "vy" : 0.5,
+        "radius" : 15,
+        "image" : "CertificateKurokawa.png",
+        "t0" : 1694818061200   
+     
+    },
+    {
         "x0" : 55,
         "y0" : 55,
         "vx" : 0.1,
@@ -142,16 +152,6 @@ var data = [
         "vy" : 0.5,
         "radius" : 15,
         "image" : "certificateMaartjeWortel-12.png",
-        "t0" : 1694818061200   
-     
-    },
-    {
-        "x0" : 1,
-        "y0" : 1,
-        "vx" : 0.5,
-        "vy" : 0.5,
-        "radius" : 15,
-        "image" : "CertificateKurokawa.png",
         "t0" : 1694818061200   
      
     }
