5894d7b0d5f30862f0f3a39c8559cdf33dd66152
noajansma <69960128+noajansma@users.noreply.github.com>
2021-09-26 10:48:19 +0200

    evanhaagen moved one lower (showing 4 times)

--- js/data.js
+++ js/data.js
@@ -1,14 +1,5 @@
 var data = [
-    {
-        "x0" : 10,
-        "y0" : 20,
-        "vx" : 0.1,
-        "vy" : 0.1,
-        "radius" : 15,
-        "image" : "CERTIFIED-3.22-evanhaagen.png",
-        "t0" : 1694818061200   
-     
-    },
+    
     {
         "x0" : 1,
         "y0" : 53,
@@ -16,6 +7,15 @@ var data = [
         "vy" : 0.3,
         "radius" : 20,
         "image" : "CERTIFIED-3.21AQMU.png",
+        "t0" : 1694818061200   
+     
+    },{
+        "x0" : 10,
+        "y0" : 20,
+        "vx" : 0.1,
+        "vy" : 0.1,
+        "radius" : 15,
+        "image" : "CERTIFIED-3.22-evanhaagen.png",
         "t0" : 1694818061200   
      
     },
