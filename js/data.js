d348565a70e6172effecff518e35f87ed4c83189
noajansma <69960128+noajansma@users.noreply.github.com>
2021-09-27 19:51:41 +0200

    karun tjin reaaaly big and 5 times. changed variables

--- js/data.js
+++ js/data.js
@@ -1,21 +1,22 @@
 var data = [
     
     {
-        "x0" : 10,
-        "y0" : 25,
-        "vx" : 0.2,
-        "vy" : 0.2,
-        "radius" : 30,
-        "image" : "Cloudcertificate-732-karuntjin.png",
-        "t0" : 1694818061200   
-     
-    },{
         "x0" : 1,
         "y0" : 20,
         "vx" : 0.1,
         "vy" : 0.1,
         "radius" : 15,
         "image" : "Certificate-322-evanhaagen.png",
+        "t0" : 1694818061200   
+     
+    },
+    {
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.2,
+        "vy" : 0.3,
+        "radius" : 20,
+        "image" : "Cloudcertificate-732-karuntjin.png",
         "t0" : 1694818061200   
      
     },
