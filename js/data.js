02987208610bbaf791f642be1c13eb505f5a8958
noajansma <69960128+noajansma@users.noreply.github.com>
2021-11-22 17:07:49 +0100

    minor showing a lot, jona not showing

--- js/data.js
+++ js/data.js
@@ -1,6 +1,15 @@
 var data = [
     
     {
+        "x0" : 2,
+        "y0" : 1,
+        "vx" : 0.3,
+        "vy" : 0.4,
+        "radius" : 20,
+        "image" : "CERTIFIED-MinorTransmedia.png",
+        "t0" : 1694818061200   
+     
+    },{
         "x0" : 1,
         "y0" : 1,
         "vx" : 0.3,
@@ -25,15 +34,6 @@ var data = [
         "vy" : 0.1,
         "radius" : 15,
         "image" : "Certificate-322-evanhaagen.png",
-        "t0" : 1694818061200   
-     
-    },{
-        "x0" : 2,
-        "y0" : 1,
-        "vx" : 0.3,
-        "vy" : 0.4,
-        "radius" : 20,
-        "image" : "CERTIFIED-MinorTransmedia.png",
         "t0" : 1694818061200   
      
     },
