b5e51b4f391df6bdfceef9b143eafd4475699ba0
noajansma <69960128+noajansma@users.noreply.github.com>
2021-11-22 17:02:44 +0100

    jona still showing a lot

--- js/data.js
+++ js/data.js
@@ -2,20 +2,20 @@ var data = [
     
     {
         "x0" : 1,
-        "y0" : 1,
-        "vx" : 0.3,
-        "vy" : 0.5,
-        "radius" : 10,
-        "image" : "CERTIFIED-82-jona.png",
-        "t0" : 1694818061200   
-     
-    },{
-        "x0" : 1,
         "y0" : 20,
         "vx" : 0.1,
         "vy" : 0.1,
         "radius" : 15,
         "image" : "Certificate-322-evanhaagen.png",
+        "t0" : 1694818061200   
+     
+    },{
+        "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.3,
+        "vy" : 0.5,
+        "radius" : 10,
+        "image" : "CERTIFIED-82-jona.png",
         "t0" : 1694818061200   
      
     },{
