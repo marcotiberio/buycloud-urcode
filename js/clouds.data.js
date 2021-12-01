6ad5521f82ca16f0bca46148cf61c5252d1f8884
André Fincato <work@andrefincato.info>
2021-12-01 15:21:24 +0100

    slow down cloud speed

--- js/clouds.data.js
+++ js/clouds.data.js
@@ -34,6 +34,8 @@ function setup() {
 
   // loop over data.js list
   cloudData.map((cloud, index) => {
+		var location = createVector(cloud.x0, cloud.y0);
+		var velocity = createVector(cloud.vx / 10, cloud.vy / 10);
 
 		var path = assetDirectory.concat(cloud.image); // Get the paths
 		var image = loadImage(path); // Load the image
