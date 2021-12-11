de0cb43189de5dfce8fa26a2d8f6b46411124e77
noajansma <69960128+noajansma@users.noreply.github.com>
2021-12-11 11:35:38 +0100

    added certificates

--- js/data.js
+++ js/data.js
@@ -2,11 +2,20 @@ var data = [
     
     {
         "x0" : 1,
+        "y0" : 1,
+        "vx" : 0.3,
+        "vy" : 0.4,
+        "radius" : 25,
+        "image" : "CERTIFIED-co1023ClaudiaKursawe.png",
+        "t0" : 1694818061200   
+     
+    },{
+        "x0" : 1,
         "y0" : 2,
         "vx" : 0.3,
         "vy" : 0.2,
         "radius" : 20,
-        "image" : "CERTIFIED-CO9-3ChristopWieland",
+        "image" : "CERTIFIED-CO9-3ChristopWieland.png",
         "t0" : 1694818061200   
      
     },{
